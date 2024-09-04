343f20e79d083ecd0354d5446ad8529029daf5b4
ixion-spacecat <ixion_spacecat@protonmail.com>
2024-09-04 00:16:33 -0700

    Add css stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/js");
+
   return {
+    passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "public",
