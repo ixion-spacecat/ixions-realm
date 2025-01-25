a554bb15b2598e8592b473aeaa7eefad645828ea
ixion-spacecat <ixion_spacecat@protonmail.com>
2025-01-25 00:59:14 -0800

    Update copyright

--- content/contact.md
+++
@@ -1,9 +0,0 @@
----
-title: Contact
----
-
-# Contact
-
-Feel free to send me an email. If I don't respond immediately, it probably means I'm hyperfixating on something and forgot that email exists.
-
-<a href="mailto:ixionspacecat@proton.me" target="_blank">ixionspacecat@proton.me</a>
---
+++ data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Ixion's Realm",
+  url: "https://ixion.neocities.org/",
+  language: "en",
+  description: "Ixion SpaceCat's personal website",
+  author: {
+    name: "Ixion SpaceCat",
+    email: "ixionspacecat@proton.me",
+  },
+  copyright: `© ${new Date().getFullYear()} Ixion SpaceCat`,
+};
